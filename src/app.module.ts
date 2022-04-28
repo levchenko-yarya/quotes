8a73c0466811d8ae8158ab50689c195c97b74874
levchenkoyarya <levchenkoyarya@gmail.com>
2022-04-28 22:35:42 +0300

    dockerfile

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,35 @@
 import { Module } from '@nestjs/common';
-import { MongooseModule } from '@nestjs/mongoose';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsModule } from './cats/cats.module';
 import { AuthModule } from './auth/auth.module';
 import { UsersService } from './users/users.service';
 import { UsersModule } from './users/users.module';
+import { getConnectionOptions } from 'typeorm';
 
 @Module({
   imports: [
-    MongooseModule.forRoot('mongodb://localhost/nest'),
-    CatsModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: '127.0.0.1',
+      port: 3307,
+      username: 'root',
+      password: 'password',
+      database: 'test',
+      entities: [],
+      synchronize: false,
+    }),
+    /*TypeOrmModule.forRootAsync({
+      useFactory: async () =>
+        Object.assign(await getConnectionOptions(), {
+          autoLoadEntities: true,
+        }),
+    }),*/
     AuthModule,
     UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService, UsersService],
 })
-export class AppModule {}
+export class AppModule {
+}
